c70cb0676de4cff330fc8d082f4b71a4c9a5d677
Facundo Carrion <facundojcarrion@gmail.com>
2023-12-06 02:02:55 -0300

    act: 6/12

--- frontend/src/components/ui/FollowButton.svelte
+++ frontend/src/components/ui/FollowButton.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { follow } from "@lib/follows/follow"
-  import { isFollowing } from "@lib/follows/isFollowing"
+  import { follow } from '@lib/follows/follow'
+  import { isFollowing } from '@lib/follows/isFollowing'
   import { follows, updateFollowCounts } from '../../store/follow.store'
 
   export let variant: 'sm' | 'md' | 'lg' | 'full' = 'md'
@@ -17,18 +17,50 @@
     following: {
       text: 'Unfollow',
       color: 'bg-red-500',
-      function: async () => {
-        const newFollowers = await follow(id_user, localStorage.getItem('user_id') as string)
-        updateFollowCounts({ followers: newFollowers.followers, followings: $follows.followings  })
+      function: async (event: MouseEvent) => {
+        if (!localStorage.getItem('user_id')) {
+          event.preventDefault()
+
+          if (confirm('Quieres loggearte para hacer esto?')) {
+            location.href = 'http://localhost:3000/auth'
+          }
+
+          return
+        }        
+
+        const newFollowers = await follow(
+          id_user,
+          localStorage.getItem('user_id') as string
+        )
+        updateFollowCounts({
+          followers: newFollowers.followers,
+          followings: $follows.followings
+        })
         following = 'notFollowing'
       }
     },
     notFollowing: {
       text: 'Follow',
       color: 'bg-blue-500',
-      function: async () => {
-        const newFollowers = await follow(id_user, localStorage.getItem('user_id') as string)
-        updateFollowCounts({ followers: newFollowers.followers, followings: $follows.followings  })
+      function: async (event: MouseEvent) => {
+        if (!localStorage.getItem('user_id')) {
+          event.preventDefault()
+
+          if (confirm('Quieres loggearte para hacer esto?')) {
+            location.href = 'http://localhost:3000/auth'
+          }
+
+          return
+        }
+
+        const newFollowers = await follow(
+          id_user,
+          localStorage.getItem('user_id') as string
+        )
+        updateFollowCounts({
+          followers: newFollowers.followers,
+          followings: $follows.followings
+        })
         following = 'following'
       }
     }
@@ -37,20 +69,18 @@
   let following: 'following' | 'notFollowing' = 'notFollowing'
 
   $: {
-    isFollowing(
-      id_user,
-      localStorage.getItem('user_id') as string
-    ).then(res => {
-      following = res.isFollow ? "following" : "notFollowing"
-    })
+    isFollowing(id_user, localStorage.getItem('user_id') as string).then(
+      res => {
+        following = res.isFollow ? 'following' : 'notFollowing'
+      }
+    )
   }
 
   const isMe = localStorage.getItem('user_id') == id_user
-
 </script>
 
 {#if !isMe}
-  <button 
+  <button
     class={`${SIZES[variant]} z-2 bg-white text-black font-bold rounded-full relative z-20 hover:bg-black hover:text-white`}
     on:click={states[following]?.function}
   >
--- frontend/src/components/ui/LogOutButton.svelte
+++ frontend/src/components/ui/LogOutButton.svelte
@@ -1,7 +1,25 @@
 <script lang='ts'>
   import { logOut } from "@lib/auth/logOut"
+
+  let logged = true
+
+  if(!localStorage.getItem('user_id')) {
+    logged = false
+  }
 </script>
 
-<button on:click={logOut} class="px-2 py-1 flex bg-red-500 font-bold rounded-sm">
-  Log Out
-</button>
+{#if logged}
+  <button
+    class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-md"
+    on:click={logOut}
+  >
+    Log Out
+  </button>
+{:else}
+  <a
+    class='px-2 py-1 font-bold text-white bg-green-500 rounded-sm'
+    href='http://localhost:3000/auth'
+  >
+    Log In
+  </a>
+{/if}
